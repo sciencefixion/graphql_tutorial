3130290d04fb8a5b6cf6dfb6397091c1e490ee47
Rion Hutsell <st.gish@gmail.com>
2020-10-02 14:23:26 -0600

    Complete steps 1-3: Setting up GraphQL with Node, Defining a Schema, and Defining Resolvers

--- server.js
+++ server.js
@@ -1,17 +1,75 @@
 var express = require('express');
-var graphqlHTTP = require('express-graphql');
+var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // Initialize a GraphQL schema
 var schema = buildSchema(`
   type Query {
-    hello: String
+    user(id: Int!): Person
+    users(shark: String): [Person]
+  },
+  type Person {
+    id: Int
+    name: String
+    age: Int
+    shark: String
   }
 `);
 
+//Sample users
+var users = [
+  {
+    id: 1,
+    name: 'Brian',
+    age: '21',
+    shark: 'Great White Shark'
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    age: '22',
+    shark: 'Whale Shark'
+  },
+  {
+    id: 3,
+    name: 'Faith',
+    age: '23',
+    shark: 'Hammerhead Shark'
+  },
+  {
+    id: 4,
+    name: 'Joseph',
+    age: '23',
+    shark: 'Tiger Shark'
+  },
+  {
+    id: 5,
+    name: 'Joy',
+    age: '25',
+    shark: 'Hammerhead Shark'
+  }
+];
+
+// Return a single user (based on id)
+var getUser = function(args) {
+  var userID = args.id;
+  return users.filter(user => user.id == userID)[0];
+}
+
+// Return a list of users (takes an optional shark parameter)
+var retrieveUsers = function(args) {
+  if (args.shark) {
+    var shark = args.shark;
+    return users.filter(user => user.shark === shark);
+  } else {
+    return users;
+  }
+}
+
 // Root resolver
 var root = {
-  hello: () => 'Hello world!'
+  user: getUser, // Resolver function to return user with specific id
+  users: retrieveUsers
 };
 
 // Create an express server and a GraphQL endpoint
